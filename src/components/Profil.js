4c1cc4b964c74a203a383a27f7b9d70e7e06682d
agrwatching <152673023+agrwatching@users.noreply.github.com>
2025-01-12 22:09:58 +0700

    Update Profil.js

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -74,7 +74,7 @@ function Profil() {
       <div className="w-[160px] md:w-[250px] p-4 md:p-5 m-5 border-4 border-purple-500 bg-black rounded-xl bg-opacity-60 shadow-xl shadow-black">
         <div className="flex flex-col items-center">
           <div className="w-12 h-12 md:w-16 md:h-16 mb-2 rounded-xl overflow-hidden border-black border-2">
-            <img src="/profil12.png" alt="icon" className="w-full h-full object-cover" />
+            <img src="/profil12.png" alt="Profile" className="w-full h-full object-cover" />
           </div>
           <p className="text-sm md:text-2xl font-bold text-center text-teal-200 font-mono">
             Wtchng Nickname
@@ -87,20 +87,18 @@ function Profil() {
           <h1
             className="text-5xl font-extrabold text-center pb-2 text-teal-300 font-mono animate-glow"
             style={{
-              fontFamily: "'Orbitron', sans-serif", // Ganti dengan font teknologi
-              textShadow: "0 0 10px rgba(0, 255, 255, 0.8), 0 0 20px rgba(0, 255, 255, 0.6), 0 0 30px rgba(0, 255, 255, 0.4)", // Efek cahaya
+              fontFamily: "'Orbitron', sans-serif",
+              textShadow: "0 0 10px rgba(0, 255, 255, 0.8), 0 0 20px rgba(0, 255, 255, 0.6), 0 0 30px rgba(0, 255, 255, 0.4)",
             }}
           >
             {currentText}
-            <span
-              className={`text-white ${showCursor ? "opacity-100" : "opacity-0"}`}
-            >
+            <span className={`text-white ${showCursor ? "opacity-100" : "opacity-0"}`}>
               |
             </span>
           </h1>
         </div>
-        <a
-          href="#Menu"
+        <button
+          onClick={scrollToMenu}  // Menambahkan event scroll
           className="text-sm md:text-lg font-bold text-center text-black font-mono hover:text-red-400 transform transition-all duration-300 group relative flex items-center justify-center overflow-hidden px-6 py-4 bg-gradient-to-r from-blue-500 to-indigo-700 rounded-full shadow-xl group-hover:from-purple-700 group-hover:to-blue-600"
         >
           <span className="absolute inset-0 w-full h-full bg-gradient-to-r from-pink-500 to-blue-500 opacity-30 rounded-full"></span>
@@ -117,7 +115,7 @@ function Profil() {
           >
             <path d="M9 18l6-6-6-6"></path>
           </svg>
-        </a>
+        </button>
       </div>
     </div>
   );
